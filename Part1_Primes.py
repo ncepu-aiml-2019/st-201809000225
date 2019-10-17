c1095dccf91eda2288c81184a9f36d3b9cfb9e56
徐俊杰 <255326339@qq,com>
2019-10-17 11:27:51 +0800

    能一行解决干嘛搞那么多幺蛾子

--- Part1_Primes.py
+++ Part1_Primes.py
@@ -1,6 +1,1 @@
-
-
-
-if __name__ == '__main__':
-    n = (int)(input("输入大于0数字，输出0到该数字之间的素数"))
-    print(" ".join("%s" % x for x in range(2, n) if not [y for y in range(2, x) if x % y == 0]))
+print(" ".join("%s" % x for x in range(2, (int)(input("输入大于0数字，输出0到该数字之间的素数"))) if not [y for y in range(2, x) if x % y == 0]))
